3bc38a53dc814ae30193cc16e84019d2f5171689
Shimul Sarker <shimul128815@gmail.com>
2025-02-05 23:21:54 +0600

    Update LibraryManagementSystem.java
    Book class done

--- files/LibraryManagementSystem.java
+++ files/LibraryManagementSystem.java
@@ -1,5 +1,49 @@
 import java.util.*;
 
+class Book {
+    private String id;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void setIssued(boolean issued) {
+        isIssued = issued;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isIssued=" + isIssued +
+                '}';
+    }
+}
+
 
 // Main class to test the library system
 public class LibraryManagementSystem {
